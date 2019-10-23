d16d5e6dd680b68fbf648e14e104fb6471e2601b
Julien Pause <julien.pause@epitech.eu>
2019-10-23 22:14:25 +0400

    Add my_putchar && my_put_nbr to libmy

--- lib/my/my.h
+++ lib/my/my.h
@@ -8,9 +8,12 @@
 #ifndef __DEF_MY__
     #define __DEF_MY__
 
+    void my_putchar(char my_char);
+
     int my_is_empty_str(char const *str);
     int my_is_digit(char const my_char);
     int my_is_space(char const my_char);
     int my_strcmp(char const *s1, char const *s2);
     int my_strlen(char const *str);
+    int my_put_nbr(int nb);
 #endif
