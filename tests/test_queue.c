f9e83571ad53a43d8c396aa76db67217e5bb22e7
Julien Pause <julien.pause@epitech.eu>
2019-10-23 23:41:58 +0400

    Add two more tests for queue_pop

--- tests/test_queue.c
+++ tests/test_queue.c
@@ -42,3 +42,31 @@ Test(queue_pop, empty_queue)
     str = queue_pop(&queue);
     cr_assert_null(str);
 }
+
+Test(queue_pop, one_pop_str_str)
+{
+    queue_t *queue = NULL;
+    char *actual = NULL;
+    char *expected = "bonsoir";
+    char *str2 = "je suis tristan bourgeois mais je suis pauvre";
+
+    queue_push(&queue, expected);
+    queue_push(&queue, str2);
+    actual = queue_pop(&queue);
+    cr_assert_str_eq(actual, expected);
+}
+
+Test(queue_pop, two_pop_char_int)
+{
+    queue_t *queue = NULL;
+    int actual = 0;
+    char my_char = '*';
+    int nb = 38;
+
+    queue_push(&queue, my_char);
+    queue_push(&queue, nb);
+    actual = queue_pop(&queue);
+    cr_assert_eq(actual, my_char);
+    actual = queue_pop(&queue);
+    cr_assert_eq(actual, nb);
+}
