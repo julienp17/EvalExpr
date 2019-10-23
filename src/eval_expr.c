2b1551cfd715bfe53e08d100ce3798ac5a59bf93
Julien Pause <julien.pause@epitech.eu>
2019-10-23 16:50:29 +0400

    Add eval_expr.c

---
+++ src/eval_expr.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2019
+** C Pool EvalExpr
+** File description:
+** Returns the result of an expression as an integer
+*/
+
+int eval_expr(char const *str)
+{
+    if (str == 0)
+        return (1);
+    return (0);
+}
