29b704629fce228a4718efee6fcba77d2fc0e09e
Julien Pause <julien.pause@epitech.eu>
2019-10-23 22:14:47 +0400

    Add main scope in main.c from pdf

--- src/main.c
+++ src/main.c
@@ -5,7 +5,14 @@
 ** Main file for EvalExpr
 */
 
-int main(void)
+#include "evalexpr.h"
+
+int main(int ac, char **av)
 {
-    return (0);
+    if (ac == 2) {
+        my_put_nbr(eval_expr(av[1]));
+        my_putchar('\n');
+        return (0);
+    }
+    return (84);
 }
