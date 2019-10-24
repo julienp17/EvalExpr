72fe30e3ecaf5ebefcdfd78a88a866cb8ebe9537
Julien Pause <julien.pause@epitech.eu>
2019-10-24 21:08:17 +0400

    Add shunting_yard and eval_stack function prototypes to queue.h

--- include/queue.h
+++ include/queue.h
@@ -15,4 +15,6 @@
 
     void queue_push(queue_t **queue_front, char *data);
     char *queue_pop(queue_t **queue_front);
+    queue_t *shunting_yard(char const *expr);
+    int eval_stack(queue_t *queue);
 #endif
