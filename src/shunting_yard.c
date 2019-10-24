047b6fdc52bfa4ac51e6175c522cf5e34d9f8fcd
Atchapa Mathias <amathias@localhost.localdomain>
2019-10-24 16:59:03 +0400

    oof

--- src/shunting_yard.c
+++ src/shunting_yard.c
@@ -7,6 +7,16 @@
 
 #include <stddef.h>
 
+int is_left_paren(char const my_char)
+{
+    return (my_char == '(');
+}
+
+int is_right_paren(char const my_char)
+{
+    return (my_char == ')');
+}
+
 char *shunting_yard(char *expr)
 {
     int i = 0;
@@ -15,21 +25,26 @@ char *shunting_yard(char *expr)
     stack_t *stack = NULL;
 
     tokens = my_expr_to_tokens(expr);
-    
     while (tokens[i] != NULL) {
-        if (my_str_isnum(tokens[i])) {
+        if (my_str_isnum(tokens[i][0]))
             queue_push(&queue, tokens[i]);
-        }
-        if (my_is_operator(tokens[i])) {
+        if (is_operator(tokens[i][0])) {
+            while (has_ge_precedence(stack->data, tokens[i][0]) && !is_left_paren(stack->data))
+                queue_push(&queue, stack_pop(&stack));
             stack_push(&stack, tokens[i]);
         }
-        while (my_is_operator) {
-            stack_pop(&queue);
-            stack_push(&queue);
+        if (is_left_paren(tokens[i][0])) {
+            stack_push(&stack, tokens[i]);
         }
+        if (is_right_paren(tokens[i][0])) {
+            while (!is_left_paren(stack->data))
+                queue_push(&queue, stack_pop(&stack));
+            if (is_left_paren(stack->data))
+                stack_pop(&stack);
+        }
+        i = i + 1;
     }
-    i++;
+    while (stack->data != NULL)
+        queue_push(&queue, stack_pop(&stack));
+    return(queue);
 }
-    
-            
-            
