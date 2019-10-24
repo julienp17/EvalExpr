984de8a21544a6f16383f2b6493dc1c995a37a63
Julien Pause <julien.pause@epitech.eu>
2019-10-24 21:11:37 +0400

    Add stack_evaluator.c

---
+++ src/stack_evaluator.c
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2019
+** C Pool EvalExpr
+** File description:
+** Handles postfix mathematical expressions
+*/
+
+#include <stddef.h>
+#include "queue.h"
+#include "stack.h"
+
+int eval_stack(queue_t *queue)
+{
+    stack_t *stack = NULL;
+    char *res = NULL;
+    char *strnum1 = NULL;
+    char *strnum2 = NULL;
+    char *op = NULL;
+
+    while (queue != NULL) {
+        while (my_is_strnum(queue->data))
+            stack_push(&stack, queue_pop(&queue));
+        op = queue_pop(&queue);
+        strnum2 = stack_pop(&stack);
+        strnum1 = stack_pop(&stack);
+        res = get_op_res_str(strnum1, get_operator(op), strnum2);
+        stack_push(&stack, res);
+    }
+    return (my_strnum_to_int(res));
+}
+
+char *get_op_res_str(char *strnum1, operator_t operator, char *strnum2)
+{
+    int nb1 = my_strnum_to_int(strnum1);
+    int nb2 = my_strnum_to_int(strnum2);
+    int res = operator.operation(nb1, nb2);
+
+    return (my_int_to_strnum(res));
+}
