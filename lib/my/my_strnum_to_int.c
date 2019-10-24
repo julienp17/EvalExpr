77b0386bd38ed9c9fadd049fb07c968c48325d6b
Julien Pause <julien.pause@epitech.eu>
2019-10-24 21:12:43 +0400

    Add my_get_denominator, my_int_to_strnum, my_intlen and my_strnum_to_int to libmy

---
+++ lib/my/my_strnum_to_int.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2019
+** C Pool EvalExpr
+** File description:
+** Converts a numerical string into an integer
+*/
+
+int my_strlen(char const *str);
+
+int strnum_to_int(char *str)
+{
+    int i = 0;
+    int nb = 0;
+    int is_neg = 0;
+
+    if (str[0] == '-') {
+        is_neg = 1;
+        i = i + 1;
+    }
+    while (i < my_strlen(str)) {
+        nb = nb * 10 + str[i] - '0';
+        i = i + 1;
+    }
+    if (is_neg)
+        nb = nb * -1;
+    return (nb);
+}
