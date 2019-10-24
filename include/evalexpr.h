06da933b70be80680412f420ee5ddb0f3adcf190
Julien Pause <julien.pause@epitech.eu>
2019-10-24 21:07:43 +0400

    Rename exp_to_tokens to expr_to_tokens

--- include/evalexpr.h
+++ include/evalexpr.h
@@ -10,13 +10,7 @@
 
     int eval_expr(char const *str);
 
-    int my_add(int a, int b);
-    int my_sub(int a, int b);
-    int my_mul(int a, int b);
-    int my_div(int a, int b);
-    int my_mod(int a, int b);
-
-    char **exp_to_tokens(char const *expr);
+    char **expr_to_tokens(char const *expr);
     int get_tokens_nb(char const *str);
     int get_next_chars_nb(char const *str, int j);
     char *get_next_token(char const *str, int *j);
