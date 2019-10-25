8bb3831f1841a6d220575d87b93debcf633f1dca
Julien Pause <julien.pause@epitech.eu>
2019-10-25 23:06:27 +0400

    Rename has_ge_precedence to has_less_precedence

--- include/evalexpr.h
+++ include/evalexpr.h
@@ -11,11 +11,9 @@
     int eval_expr(char const *str);
 
     char **expr_to_tokens(char const *expr);
-    int get_tokens_nb(char const *str);
+    int get_tokens_nb(char const *expr);
     int get_next_chars_nb(char const *str, int j);
     char *get_next_token(char const *str, int *j);
 
-    int is_left_paren(char const my_char);
-    int is_right_paren(char const my_char);
-    int has_ge_precedence(char symbol1, char symbol2);
+    int has_less_precedence(char symbol1, char symbol2);
 #endif
