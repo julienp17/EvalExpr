a847ff316777fbd039efbe38cb454aac72f953ca
Julien Pause <julien.pause@epitech.eu>
2019-10-26 16:10:00 +0400

    I did it !

--- src/expr_to_tokens.c
+++ src/expr_to_tokens.c
@@ -68,10 +68,10 @@ static int minus_is_operator(char const *expr, int i)
 {
     if (i == 0)
         return (0);
-    if (my_is_digit(expr[(i - 1)]))
-        return (1);
-    else
+    if (!my_is_digit(expr[(i - 1)]) && my_is_digit(expr[(i + 1)]))
         return (0);
+    else
+        return (1);
 }
 
 char *get_next_token(char const *expr, int *j)
