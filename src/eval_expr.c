79c4d6fedd18da31f522b70cb434df0ee735d570
Julien Pause <julien.pause@epitech.eu>
2019-10-27 17:59:57 +0400

    Add support when there is no expr but only one number

--- src/eval_expr.c
+++ src/eval_expr.c
@@ -6,13 +6,20 @@
 */
 
 #include <stddef.h>
+#include "evalexpr.h"
 #include "my.h"
 #include "queue.h"
 
 int eval_expr(char const *str)
 {
     queue_t *output_queue = NULL;
+    char *expr = NULL;
+    char **tokens = NULL;
 
-    output_queue = shunting_yard(my_strdup_ns(str));
+    expr = my_strdup_ns(str);
+    if (get_tokens_nb(expr) == 1)
+        return (my_strnum_to_int(expr));
+    tokens = expr_to_tokens(expr);
+    output_queue = shunting_yard(tokens);
     return (eval_stack(&output_queue));
 }
